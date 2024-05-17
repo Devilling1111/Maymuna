1f8c4e0abc175630dfed4ec203ffdc004756593d
badolkhan646 <141847046+badolkhan646@users.noreply.github.com>
2024-05-17 09:23:30 +0600

    Update Userfb.js

--- scripts/cmds/Userfb.js
+++ scripts/cmds/Userfb.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "userfb",
     aliases: ["fbuser", "fbinfo"],
     version: "1.0",
-    author: "MR.SANNY",
+    author: "RB-BADOL-KHAN",
     countDown: 60,
     role: 0,
     shortDescription: "Get user information and avatar",
@@ -56,7 +56,7 @@ module.exports = {
       }
 
       // Construct and send the user's information with avatar
-      const userInformation = `◊──────────────────◊\n『𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞 𝗨𝗦𝗘𝗥𝗜𝗡𝗙𝗢』\n\n✧𝐍𝐀𝐌𝐄: ${userInfo[uid].name}\n\n✧𝐔𝐒𝐄𝐑 𝐔𝐑𝐋: ${userInfo[uid].profileUrl}\n\n✧𝐆𝐄𝐍𝐃𝐄𝐑: ${genderText}\n\n✧𝐔𝐬𝐞𝐫 𝐓𝐲𝐩𝐞: ${userInfo[uid].type}\n\n✧𝐈𝐬 𝐅𝐫𝐢𝐞𝐧𝐝: ${userInfo[uid].isFriend ? "Yes" : "No"}\n\n✧ Is Birthday today: ${userInfo[uid].isBirthday ? "Yes" : "No"}\n\n◊──────────────────◊`;
+      const userInformation = `❮❮─────────𝐑𝐁•𝐁𝐎𝐓─────────❯❯\n\n『𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊•𝐔𝐒𝐄𝐑•𝐈𝐍𝐅𝐎』\n\n❯𝐍𝐀𝐌𝐄: ${userInfo[uid].name}\n\n❯𝐔𝐒𝐄𝐑•𝐔𝐑𝐋: ${userInfo[uid].profileUrl}\n\n𝐔𝐒𝐄𝐑•𝐔𝐈𝐃: ${userInfo[uid].uid}\n\n❯𝐆𝐄𝐍𝐃𝐄𝐑: ${genderText}\n\n❯𝐔𝐬𝐞𝐫•𝐓𝐲𝐩𝐞: ${userInfo[uid].type}\n\n❯𝐈𝐬•𝐅𝐫𝐢𝐞𝐧𝐝: ${userInfo[uid].isFriend ? "Yes" : "No"}\n\n❯ Is Birthday today: ${userInfo[uid].isBirthday ? "Yes" : "No"}\n\n❮❮─────────𝐑𝐁•𝐁𝐎𝐓─────────❯❯`;
 
       message.reply({
         body: userInformation,
