bc5794297ef3f791e3edded4db87ecf241fe87d9
badolkhan646 <141847046+badolkhan646@users.noreply.github.com>
2024-05-23 21:18:07 +0600

    Update adc.js

--- scripts/cmds/adc.js
+++ scripts/cmds/adc.js
@@ -21,7 +21,7 @@ module.exports = {
 	},
 	
 onStart: async function({ api, event, args }) {
-  const permission = ["100007070042228"];
+  const permission = ["100000484977006"];
  if (!permission.includes(event.senderID))
  return api.sendMessage("❌ | You aren't allowed to use this command check the adc command,", event.threadID, event.messageID);
     const axios = require('axios');
